c0a824cabfc40c7f86d0f6c64671c0eafaa06829
niklas-glaeser <123083964+niklas-glaeser@users.noreply.github.com>
2024-01-27 12:03:10 +0100

    added dropdown menu to user Profile

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function guestLogin() {
     window.location.href = 'summary.html';
 }
+
+function toggleDropdown() {
+    let dropdownContent = document.getElementById("dropdownContent");
+    if (dropdownContent.style.display === "flex") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "flex";
+    }
+}
+
+document.addEventListener("click", function(event) {
+    var userProfile = document.getElementById("userProfile");
+    var dropdownContent = document.getElementById("dropdownContent");
+
+    if (event.target !== userProfile && event.target !== dropdownContent) {
+        dropdownContent.style.display = "none";
+    }
+});
