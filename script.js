bc9a4395286af1405ec86c3c7db550396f38f44f
niklas-glaeser <123083964+niklas-glaeser@users.noreply.github.com>
2024-02-05 16:22:40 +0100

    changed header and sidebar to w3-include

--- script.js
+++ script.js
@@ -27,23 +27,37 @@ function closeSignUpForm() {
     signUpForm.style.display = "none";
 }
 
-
+/*
 async function deleteAllUser() {
     users.splice(0, users.length);
     await setItem("users", JSON.stringify(users));
 }
 
+*/
 
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      file = elmnt.getAttribute("w3-include-html");
+      if (file) {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+            elmnt.removeAttribute("w3-include-html");
+            includeHTML();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        return;
+      }
+    }
+  }
 
-function renderHeader() {
-    const header = document.getElementById('headerContainer');
-
-    header.innerHTML = headerTemplate();
-
-}
-
-function renderSidebar() {
-    const sidebar = document.getElementById('sidebarContainer');
-
-    sidebar.innerHTML = sidebarTemplate();
-}
+  function init() {
+    includeHTML();
+  }
