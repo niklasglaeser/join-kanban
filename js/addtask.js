c8d94520f911be6836783f98820a33d631c50d2c
niklas-glaeser <123083964+niklas-glaeser@users.noreply.github.com>
2024-02-12 14:07:31 +0100

    adds first style of addTask

---
+++ js/addtask.js
@@ -0,0 +1,12 @@
+function changeClearButtonIcon() {
+    let icon = document.getElementById("clearButtonIcon");
+  
+    icon.src = "../img/clear-button-icon-alternative.svg";
+}
+
+function changeClearButtonIconBack() {
+    let icon = document.getElementById("clearButtonIcon");
+  
+    icon.src = "../img/clear-button-icon.svg";
+}
+
