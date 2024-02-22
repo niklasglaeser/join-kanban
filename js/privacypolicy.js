306e0f7ae42864266d339530ba0160bfa0f8f1ad
Marco Engelhardt <sirmarco@reussnet.ch>
2024-02-22 18:24:30 +0100

    fixes legal notice

--- js/privacypolicy.js
+++ js/privacypolicy.js
@@ -1,10 +1,16 @@
-const queryString = window.location.search;
+let queryString = window.location.search;
 let urlParams = new URLSearchParams(queryString);
 let extern = urlParams.get("extern");
 
-window.addEventListener("load", (event) => {
+window.addEventListener("load", () => {
   if (extern == "yes") {
     setTimeout(function () {
+      document.getElementById("link-privacy").href =
+        "../pages/privacypolicy.html?extern=yes";
+      document.getElementById("link-privacy").setAttribute("target", "_blank");
+      document.getElementById("link-legal").href =
+        "../pages/legalnotice.html?extern=yes";
+      document.getElementById("link-legal").setAttribute("target", "_blank");
       document.getElementById("nav-menu").style.display = "none";
       document.getElementById("headerRight").style.display = "none";
       document.getElementById("link").href = "../index.html";
